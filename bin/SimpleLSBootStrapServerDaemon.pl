7f02efc95ecf41791cf522f4c9a960a927e11aaf
sowmya <sowmya@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2013-02-12 00:20:17 +0000

    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/alake-newlsreg@5542 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

---
+++ bin/SimpleLSBootStrapServerDaemon.pl
@@ -0,0 +1,254 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+SimpleLSBootStrapServerDaemon.pl - Detrmines lookup service to be used by this host
+
+=head1 DESCRIPTION
+
+This daemon determines lookup service to be used by host.
+
+=cut
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+
+use perfSONAR_PS::Common;
+use perfSONAR_PS::Utils::Daemon qw/daemonize setids lockPIDFile unlockPIDFile/;
+use perfSONAR_PS::Utils::NetLogger;
+use SimpleLSBootStrap::ServerDaemonHandler;
+
+use Getopt::Long;
+use Config::General;
+use Log::Log4perl qw/:easy/;
+
+# set the process name
+$0 = "SimpleLSBootStrapServerDaemon.pl";
+
+my @child_pids = ();
+
+$SIG{INT}  = \&signalHandler;
+$SIG{TERM} = \&signalHandler;
+
+my $CONFIG_FILE;
+my $LOGOUTPUT;
+my $LOGGER_CONF;
+my $PIDFILE;
+my $DEBUGFLAG;
+my $HELP;
+my $RUNAS_USER;
+my $RUNAS_GROUP;
+
+my ( $status, $res );
+
+$status = GetOptions(
+    'config=s'  => \$CONFIG_FILE,
+    'output=s'  => \$LOGOUTPUT,
+    'logger=s'  => \$LOGGER_CONF,
+    'pidfile=s' => \$PIDFILE,
+    'verbose'   => \$DEBUGFLAG,
+    'user=s'    => \$RUNAS_USER,
+    'group=s'   => \$RUNAS_GROUP,
+    'help'      => \$HELP
+);
+
+if ( not $CONFIG_FILE ) {
+    print "Error: no configuration file specified\n";
+    exit( -1 );
+}
+
+my %conf = Config::General->new( $CONFIG_FILE )->getall();
+
+if ( not $PIDFILE ) {
+    $PIDFILE = $conf{"pid_file"};
+}
+
+if ( not $PIDFILE ) {
+    $PIDFILE = "/var/run/SimpleLSBootStrapServerDaemon.pid";
+}
+
+( $status, $res ) = lockPIDFile( $PIDFILE );
+if ( $status != 0 ) {
+    print "Error: $res\n";
+    exit( -1 );
+}
+
+my $fileHandle = $res;
+
+# Check if the daemon should run as a specific user/group and then switch to
+# that user/group.
+if ( not $RUNAS_GROUP ) {
+    if ( $conf{"group"} ) {
+        $RUNAS_GROUP = $conf{"group"};
+    }
+}
+
+if ( not $RUNAS_USER ) {
+    if ( $conf{"user"} ) {
+        $RUNAS_USER = $conf{"user"};
+    }
+}
+
+if ( $RUNAS_USER and $RUNAS_GROUP ) {
+    if ( setids( USER => $RUNAS_USER, GROUP => $RUNAS_GROUP ) != 0 ) {
+        print "Error: Couldn't drop priviledges\n";
+        exit( -1 );
+    }
+}
+elsif ( $RUNAS_USER or $RUNAS_GROUP ) {
+
+    # they need to specify both the user and group
+    print "Error: You need to specify both the user and group if you specify either\n";
+    exit( -1 );
+}
+
+# Now that we've dropped privileges, create the logger. If we do it in reverse
+# order, the daemon won't be able to write to the logger.
+my $logger;
+if ( not defined $LOGGER_CONF or $LOGGER_CONF eq q{} ) {
+    use Log::Log4perl qw(:easy);
+
+    my $output_level = $INFO;
+    if ( $DEBUGFLAG ) {
+        $output_level = $DEBUG;
+    }
+
+    my %logger_opts = (
+        level  => $output_level,
+        layout => '%d (%P) %p> %F{1}:%L %M - %m%n',
+    );
+
+    if ( defined $LOGOUTPUT and $LOGOUTPUT ne q{} ) {
+        $logger_opts{file} = $LOGOUTPUT;
+    }
+
+    Log::Log4perl->easy_init( \%logger_opts );
+    $logger = get_logger( "SimpleLSBootStrap" );
+}
+else {
+    use Log::Log4perl qw(get_logger :levels);
+
+    my $output_level = $INFO;
+    if ( $DEBUGFLAG ) {
+        $output_level = $DEBUG;
+    }
+
+    my %logger_opts = (
+        level  => $output_level,
+        layout => '%d (%P) %p> %F{1}:%L %M - %m%n',
+    );
+
+    if ( $LOGOUTPUT ) {
+        $logger_opts{file} = $LOGOUTPUT;
+    }
+
+    Log::Log4perl->init( $LOGGER_CONF );
+    $logger = get_logger( "SimpleLSBootStrap" );
+    $logger->level( $output_level ) if $output_level;
+}
+
+#BEGIN read configuration
+$logger->info( perfSONAR_PS::Utils::NetLogger::format( "SimpleLSBootStrapServerDaemon.init.start") );
+if ( not $conf{"hosts_file"} ) {
+    my $log_msg = perfSONAR_PS::Utils::NetLogger::format( "SimpleLSBootStrapServerDaemon.init.end", 
+        { status => -1, 
+          msg => "You must specify a hosts file with the hosts_file property"
+        });
+    $logger->error( $log_msg );
+    exit(-1);
+}
+if ( not $conf{"output_file"} ) {
+    my $log_msg = perfSONAR_PS::Utils::NetLogger::format( "SimpleLSBootStrapServerDaemon.init.end", 
+        { status => -1, 
+          msg => "You must specify an output file with the output_file property"
+        });
+    $logger->error( $log_msg );
+    exit(-1);
+}
+if ( not $conf{"update_interval"} ) {
+    $conf{"update_interval"} = 3600;
+}
+#END read configuration
+
+if ( not $DEBUGFLAG ) {
+    ( $status, $res ) = daemonize();
+    if ( $status != 0 ) {
+        my $log_msg = perfSONAR_PS::Utils::NetLogger::format( "SimpleLSBootStrapServerDaemon.init.end", 
+        { status => -1, 
+          msg => "Couldn't daemonize: " . $res 
+        });
+        $logger->error( $log_msg );
+        exit( -1 );
+    }
+}
+
+unlockPIDFile( $fileHandle );
+
+#BEGIN handler
+my $handler = new SimpleLSBootStrap::ServerDaemonHandler();
+$handler->init( \%conf );
+$logger->info( perfSONAR_PS::Utils::NetLogger::format( "SimpleLSBootStrapServerDaemon.init.end") );
+
+while(1){
+    $handler->handle();
+}
+#END handler
+exit( 0 );
+
+sub signalHandler {
+    exit( 0 );
+}
+
+__END__
+
+=head1 SEE ALSO
+
+L<FindBin>, L<Getopt::Long>, L<Config::General>, L<Log::Log4perl>,
+L<perfSONAR_PS::Common>, L<perfSONAR_PS::Utils::Daemon>,
+L<perfSONAR_PS::Utils::Host>, L<SimpleLSBootStrap::ServerDaemonHandler>
+
+To join the 'perfSONAR Users' mailing list, please visit:
+
+  https://mail.internet2.edu/wws/info/perfsonar-user
+
+The perfSONAR-PS subversion repository is located at:
+
+  http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk
+
+Questions and comments can be directed to the author, or the mailing list.
+Bugs, feature requests, and improvements can be directed here:
+
+  http://code.google.com/p/perfsonar-ps/issues/list
+
+=head1 VERSION
+
+$Id: daemon.pl 3949 2010-03-12 18:04:21Z alake $
+
+=head1 AUTHOR
+
+Andy Lake, andy@es.net
+
+=head1 LICENSE
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2010, Internet2
+
+All rights reserved.
+
+=cut
---
+++ perl-perfSONAR_PS-SimpleLSBootStrap.spec
@@ -0,0 +1,121 @@
+%define _unpackaged_files_terminate_build      0
+%define install_base /opt/SimpleLS/bootstrap
+%define init_script_server simple_ls_bootstrap_server
+%define init_script_client simple_ls_bootstrap_client
+
+%define relnum 0
+%define disttag pSPS
+
+Name:           perl-perfSONAR_PS-SimpleLS-BootStrap
+Version:        3.3
+Release:        %{relnum}.%{disttag}
+Summary:        perfSONAR_PS SimpleLS BootStrap
+License:        distributable, see LICENSE
+Group:          Development/Libraries
+Source0:        perfSONAR_PS-SimpleLS-BootStrap-%{version}.%{relnum}.tar.gz
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildArch:      noarch
+
+%description
+The perfSONAR_PS SimpleLS BootStrap is used to monitor/retrieve currently active Simple LS nodes.
+
+%package server
+Summary:  Package for host to determine and publish active list of Lookup Services
+Requires:               perl(FindBin)
+Requires:               perl(Getopt::Long)
+Requires:               perl(JSON)
+Requires:               perl(LWP)
+Requires:               perl(LWP::Simple)
+Requires:               perl(Net::Ping)
+Requires:               perl(Params::Validate)
+Requires:               perl(Time::HiRes)
+Requires:               perl(URI)
+Requires:               perl(YAML::Syck)
+Requires:               perl(DateTime::Format::ISO8601)
+Requires:       perl
+Requires:       coreutils
+Requires:       shadow-utils
+Requires:       chkconfig
+
+%description server
+Package for host to determine and publish active list of Lookup Services
+
+%package client
+Summary:  Package for host running Lookup service clients to determine best Simple Lookup Service to use
+Requires:               perl(FindBin)
+Requires:               perl(Getopt::Long)
+Requires:               perl(JSON)
+Requires:               perl(LWP)
+Requires:               perl(LWP::Simple)
+Requires:               perl(Net::Ping)
+Requires:               perl(Params::Validate)
+Requires:               perl(Time::HiRes)
+Requires:               perl(URI)
+Requires:               perl(YAML::Syck)
+Requires:               perl(DateTime::Format::ISO8601)
+Requires:       perl
+Requires:       coreutils
+Requires:       shadow-utils
+Requires:       chkconfig
+
+%description client
+Package for host running Lookup service clients to determine best Simple Lookup Service to use
+
+%pre
+/usr/sbin/groupadd perfsonar 2> /dev/null || :
+/usr/sbin/useradd -g perfsonar -r -s /sbin/nologin -c "perfSONAR User" -d /tmp perfsonar 2> /dev/null || :
+
+%prep
+%setup -q -n perfSONAR_PS-SimpleLS-BootStrap-%{version}.%{relnum}
+
+%build
+
+%install
+rm -rf $RPM_BUILD_ROOT
+
+make ROOTPATH=$RPM_BUILD_ROOT/%{install_base} rpminstall
+
+mkdir -p $RPM_BUILD_ROOT/etc/init.d
+
+awk "{gsub(/^PREFIX=.*/,\"PREFIX=%{install_base}\"); print}" scripts/%{init_script_server} > scripts/%{init_script_server}.new
+install -D -m 755 scripts/%{init_script_server}.new $RPM_BUILD_ROOT/etc/init.d/%{init_script_server}
+awk "{gsub(/^PREFIX=.*/,\"PREFIX=%{install_base}\"); print}" scripts/%{init_script_client} > scripts/%{init_script_client}.new
+install -D -m 755 scripts/%{init_script_client}.new $RPM_BUILD_ROOT/etc/init.d/%{init_script_client}
+
+%post server
+mkdir -p /var/log/SimpleLS
+chown perfsonar:perfsonar /var/log/SimpleLS
+/sbin/chkconfig --add %{init_script_server}
+
+%post client
+mkdir -p /var/log/SimpleLS
+chown perfsonar:perfsonar /var/log/SimpleLS
+/sbin/chkconfig --add %{init_script_client}
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files server
+%defattr(-,perfsonar,perfsonar,-)
+%config %{install_base}/etc/hosts-server.yml
+%config %{install_base}/etc/activehosts.json
+%config %{install_base}/etc/SimpleLSBootStrapServerDaemon.conf
+%config %{install_base}/etc/SimpleLSBootStrapServerDaemon-logger.conf
+%attr(0755,perfsonar,perfsonar) /etc/init.d/%{init_script_server}
+%attr(0755,perfsonar,perfsonar) %{install_base}/scripts/%{init_script_server}
+%attr(0755,perfsonar,perfsonar) %{install_base}/bin/SimpleLSBootStrapServerDaemon.pl
+%attr(0755,perfsonar,perfsonar) %{install_base}/bin/SimpleLSBootStrap.pl
+%attr(0755,perfsonar,perfsonar) %{install_base}/lib/*
+%{install_base}/dependencies
+%{install_base}/doc/*
+
+%files client
+%defattr(-,perfsonar,perfsonar,-)
+%config %{install_base}/etc/hosts-client.yml
+%config %{install_base}/etc/service_url
+%config %{install_base}/etc/SimpleLSBootStrapClientDaemon.conf
+%config %{install_base}/etc/SimpleLSBootStrapClientDaemon-logger.conf
+%attr(0755,perfsonar,perfsonar) /etc/init.d/%{init_script_client}
+%attr(0755,perfsonar,perfsonar) %{install_base}/scripts/%{init_script_client}
+%attr(0755,perfsonar,perfsonar) %{install_base}/bin/SimpleLSBootStrapClientDaemon.pl
+%attr(0755,perfsonar,perfsonar) %{install_base}/lib/*
